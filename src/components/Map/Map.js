b9b7e189561e48af893e78f047b2a220308f7d17
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-08-23 15:32:54 +0200

    The map lock itself when the user is scrolling

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,21 +1,33 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, useCallback } from "react";
 import PropTypes from "prop-types";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-unresolved
 import MapboxLanguage from "@mapbox/mapbox-gl-language";
+import useScrollContext from "../../context/Scroll/ScrollContext.js";
 import Marker from "./Marker/Marker.js";
+import { eventOnElement } from "../../global/Functions.js";
 import { Map as MapConfig } from "../../config/config.js";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./Map.css";
 
 mapboxgl.accessToken = MapConfig.apiKey;
 
-function Map({ lat, lng, zoom: zoomLevel, markers }) {
+function Map({ lat, lng, zoom, markers }) {
 	/* ---- States ---------------------------------- */
+	const scroll = useScrollContext();
 	const mapWrapper = useRef(null);
 	const map = useRef(null);
-	const [latitude, setLatitude] = useState(lat);
-	const [longitude, setLongitude] = useState(lng);
-	const [zoom, setZoom] = useState(zoomLevel);
+	const [markersJSX, setMarkersJSX] = useState([]);
+	const [isInteractive, setInteractive] = useState(false);
+
+	/* ---- Functions ------------------------------- */
+	const disableInteractiveState = () => { setInteractive(false); };
+	const toggleInteractiveState = () => { setInteractive(!isInteractive); };
+
+	const onScroll = useCallback(() => {
+		if (isInteractive) {
+			disableInteractiveState();
+		}
+	}, [isInteractive]);
 
 	/* ---- Effects --------------------------------- */
 	useEffect(() => {
@@ -24,44 +36,51 @@ function Map({ lat, lng, zoom: zoomLevel, markers }) {
 			map.current = new mapboxgl.Map({
 				container: mapWrapper.current,
 				style: "mapbox://styles/mapbox/streets-v11",
-				center: [longitude, latitude],
+				center: [lng, lat],
 				zoom,
 				language: "auto"
 			});
 			map.current.addControl(new MapboxLanguage({}));
 
-			// Events
-			map.current.on("move", () => {
-				const center = map.current.getCenter();
-
-				setLongitude(center.lng.toFixed(4));
-				setLatitude(center.lat.toFixed(4));
-				setZoom(map.current.getZoom().toFixed(2));
-			});
+			// Markers
+			setMarkersJSX(markers.features.map(feature => (
+				<Marker
+					key={`map-marker-${feature.geometry.coordinates[0]}-${feature.geometry.coordinates[1]}`}
+					map={map.current}
+					feature={feature}
+				/>
+			)));
 		}
 
 		return () => {
 			map.current.remove();
 			map.current = null;
+
+			setMarkersJSX([]);
 		};
 		// This hook must run once at page loading
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
+	useEffect(() => {
+		scroll.addListener(onScroll);
+		return () => { scroll.removeListener(onScroll); };
+	}, [scroll, onScroll]);
+
 	/* ---- Page content ---------------------------- */
 	return (
 		<div className="map">
+			<div className={`map-interactive-status${isInteractive ? " interactive" : ""}`} {...eventOnElement(toggleInteractiveState)}>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+					<path d="M23.19 32C28.86 32 34.34 34.08 38.59 37.86L312.6 281.4C317.3 285.6 320 291.6 320 297.9C320 310.1 310.1 320 297.9 320H179.8L236.6 433.7C244.5 449.5 238.1 468.7 222.3 476.6C206.5 484.5 187.3 478.1 179.4 462.3L121.2 346L38.58 440.5C34.4 445.3 28.36 448 22.01 448C9.855 448 0 438.1 0 425.1V55.18C0 42.38 10.38 32 23.18 32H23.19z"/>
+				</svg>
+			</div>
+
 			<div className="map-box" ref={mapWrapper}/>
 
-			{(map.current && markers) && (
+			{(map.current && markersJSX) && (
 				<div className="markers">
-					{markers.features.map(feature => (
-						<Marker
-							key={`map-marker-${feature.geometry.coordinates[0]}-${feature.geometry.coordinates[1]}`}
-							map={map.current}
-							feature={feature}
-						/>
-					))}
+					{markersJSX}
 				</div>
 			)}
 		</div>
