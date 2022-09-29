363939a16370a46499f1780d547d0befd9db8f3f
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-09-29 11:23:29 +0200

    Add a warning message in the console

--- src/index.js
+++ src/index.js
@@ -21,6 +21,18 @@ root.render(
 	</DocumentTitleProvider>
 );
 
+// Print a custom message to the console
+const commonCSS = "font-family: \"Noto sans\", \"Segoe UI\", sans-serif;";
+const titleCSS = `${commonCSS} font-size: 2.5em; padding: 20px 0; color: #C91F37; font-weight: bold;`;
+console.log("%c///   %cRestricted Area   %c///", `${titleCSS} padding-left: 30px;`, titleCSS, titleCSS);
+
+console.log("%cCet endroit ne sert qu'aux développeurs. Écrire ou coller quelque chose ici pourrait compromettre vos données. Veuillez fermer cette fenêtre si vous n'êtes pas absolument certain de ce que vous faites.", commonCSS);
+
+const secretCSS = `${commonCSS} font-size: 0.7em; color: rgba(255, 255, 255, 0.3)`;
+console.log("%cSecret code: %c68 74 74 70 73 3a 2f 2f 77 77 77 2e 79 6f 75 74 75 62 65 2e 63 6f 6d 2f 77 61 74 63 68 3f 76 3d 64 51 77 34 77 39 57 67 58 63 51", `font-weight: bold; ${secretCSS}`, secretCSS);
+
+console.log("%c//////////////////////////", `${titleCSS} padding-left: 30px;`);
+
 // Disable the console in production
 if (process.env.NODE_ENV === "production") {
 	/* eslint-disable no-unused-vars */
